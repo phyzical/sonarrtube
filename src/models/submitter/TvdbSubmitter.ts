985ebb7cefc04812ac252a61634ec0e85e4e3baf
phyzical <5182053+phyzical@users.noreply.github.com>
2024-06-29 18:49:55 +0800

    rejig the upload logic, checkf or bans and support cropping to avoid bans

--- src/models/submitter/TvdbSubmitter.ts
+++ src/models/submitter/TvdbSubmitter.ts
@@ -1,13 +1,14 @@
 import { cleanTextContainsXpath, delay } from '../../helpers/Puppeteer.js';
 import { log } from '../../helpers/Log.js';
 import { setTimeout } from 'node:timers/promises';
-import { createWriteStream } from 'node:fs';
-import fetch from 'node-fetch';
-import { cachePath } from '../../helpers/Cache.js';
 import { BaseSubmitter } from './BaseSubmitter.js';
+import { ElementHandle } from 'puppeteer';
+import { unlinkSync } from 'node:fs';
+import { processThumbnail } from '../../helpers/Thumbnails.js';
 
 export class TvdbSubmitter extends BaseSubmitter {
   #baseURL = 'https://thetvdb.com';
+  imageUploadsDisabled = null;
 
   getEpisodeXpath(episodeTitle: string): string {
     return `xpath///tr[.//a[${cleanTextContainsXpath(episodeTitle)}]]/td`;
@@ -80,23 +81,31 @@ export class TvdbSubmitter extends BaseSubmitter {
     log(`opened ${showSeriesURL}`, true);
   }
 
-  async openEditEpisodePage(): Promise<void> {
+  async openEpisodePage(edit: boolean = true): Promise<void> {
     const episodeTitle = this.video.youtubeVideo.title();
     const series = this.video.tvdbSeries.slug;
+    const editEpisodeXpath = 'xpath///*[contains(text(),"Edit Episode")]';
     if (!this.video.missingFromTvdb()) {
       const showSeriesURL = [
-        this.#baseURL, 'series', series, 'episodes', this.video.tvdbEpisode.id, '0', 'edit'
-      ].join('/');
+        this.#baseURL, 'series', series, 'episodes', this.video.tvdbEpisode.id,
+      ].concat(edit ? ['0', 'edit'] : []).join('/');
       await this.goto(showSeriesURL);
-      await this.find(`xpath///*[contains(text(), "Episode ${this.video.tvdbEpisode.number}")]`);
+
+      if (edit) {
+        await this.find(`xpath///*[contains(text(), "Episode ${this.video.tvdbEpisode.number}")]`);
+      } else {
+        await this.find(editEpisodeXpath);
+      }
+
       await this.find(`xpath///*[contains(text(), "Season ${this.video.tvdbEpisode.seasonNumber}")]`);
+
     } else {
       await delay(500);
       await this.openSeriesSeasonPage();
       await this.click(this.getEpisodeXpath(episodeTitle));
-      await this.click('xpath///*[contains(text(),"Edit Episode")]');
+      await this.click(editEpisodeXpath);
     }
-    log(`opened editEpisodePage ${episodeTitle}`, true);
+    log(`opened EpisodePage ${episodeTitle}`, true);
   }
 
   async verifyAddedEpisode(): Promise<string> {
@@ -163,28 +172,90 @@ export class TvdbSubmitter extends BaseSubmitter {
 
   }
 
-  private async uploadEpisodeThumbnail(): Promise<void> {
-    const episode = this.video.youtubeVideo;
-    log('Starting image upload', true);
-    const thumbnailUrl = episode.thumbnail;
-    const thumbnailPath = `${cachePath('thumbnails/')}/${episode.id}.jpg`;
-    await fetch(thumbnailUrl).then((res) =>
-      res.body.pipe(createWriteStream(thumbnailPath))
-    );
+  private async checkForUploadBan(): Promise<void> {
+    if (this.imageUploadsDisabled == null) {
+      try {
+        if (await this.find(
+          'xpath///*[contains(text(), "your account has been temporarily restricted from uploading")]'
+        )) {
+          this.imageUploadsDisabled = true;
+        }
+        // eslint-disable-next-line no-empty
+      } catch (_e) { }
+    }
+  }
 
-    await this.click('xpath///a[text()=\'Add Artwork\']');
+  private async handleCropperTool(): Promise<void> {
+    await this.page.evaluate(() => {
+      const cropperInstance = window.cropper.cropper;
+      const imageData = cropperInstance.getImageData();
+      const canvasData = cropperInstance.getCanvasData();
+
+      cropperInstance.setCropBoxData({
+        left: canvasData.left,
+        top: canvasData.top,
+        width: imageData.naturalWidth,
+        height: imageData.naturalHeight
+      });
+    });
+  }
+
+  private async uploadEpisodeThumbnail(count: number = 0): Promise<string> {
+    if (this.imageUploadsDisabled == true) {
+      log('Image uploads disabled, skipping');
+
+      return 'FAILED';
+    }
+
+    const episode = this.video.youtubeVideo;
+    log(`Starting image upload attempt ${count + 1}`, true);
+    const thumbnailUrl = episode.thumbnail;
+
+    let thumbnailPath;
+
     try {
-      const fileSelector = 'input[name=\'file\']';
-      const elementHandle = await this.find(fileSelector);
-      await elementHandle[0].uploadFile(thumbnailPath);
-      await setTimeout(3000);
-      await this.click('xpath///button[text()=\'Continue\']');
-      await this.find(`xpath///*[contains(text(),"${episode.title()}")]`);
-      log('Successfully uploaded image', true);
+      await this.click('xpath///a[text()=\'Add Artwork\']');
+
+      await this.checkForUploadBan();
+      if (this.imageUploadsDisabled == true) {
+        log('Image uploads disabled, skipping');
+
+        return 'FAILED';
+      }
+
+      try {
+        thumbnailPath = await processThumbnail(thumbnailUrl, episode.id, count);
+      } catch (_e) {
+        try {
+          log('Trying again...');
+          thumbnailPath = await processThumbnail(thumbnailUrl, episode.id, count);
+        } catch (__e) {
+          log('Trying one more time...');
+          thumbnailPath = await processThumbnail(thumbnailUrl, episode.id, count);
+        }
+      }
+
+      if (thumbnailPath) {
+        const elementHandle = await this.find('input[name=\'file\']') as ElementHandle<HTMLInputElement>;
+        await elementHandle.uploadFile(thumbnailPath);
+        await setTimeout(3000);
+        await this.click('xpath///button[text()=\'Continue\']');
+        await this.loaded();
+        await this.handleCropperTool();
+        await this.click('xpath///button[text()=\'Finish\']');
+        await this.loaded();
+        unlinkSync(thumbnailPath);
+        await this.find(`xpath///*[contains(text(),"${episode.title()}")]`);
+        log('Successfully uploaded image');
+      }
+
+      return thumbnailPath;
     } catch (e) {
       log(e);
       // await this.takeScreenshot();
       log('Failed image upload');
+
+      return 'FAILED';
     }
   }
 
@@ -206,7 +277,7 @@ export class TvdbSubmitter extends BaseSubmitter {
         const addEpisodeSelector = 'xpath///*[contains(text(),"Whoops, looks like something went wrong")]';
         await this.find(addEpisodeSelector);
         try {
-          await this.openEditEpisodePage();
+          await this.openEpisodePage(true);
         } catch (e) {
           log(e);
         }
@@ -223,9 +294,26 @@ export class TvdbSubmitter extends BaseSubmitter {
     }
   }
 
-  async backfillEpisode(): Promise<void> {
-    await this.openEditEpisodePage();
+  async backfillEpisodeProductionCode(): Promise<void> {
+    await this.openEpisodePage(true);
     await this.updateEpisode();
+  }
+
+  async backfillEpisodeImage(): Promise<void> {
+    const backfillAttempts = this.video.thumbnailUploadAttemptCount();
+    // then two contrasts attempting to remove text
+    // then once more setting no max length of text to 1
+    // finally if all else fails just use raw
+    if (this.video.thumbnailUploadAttemptCount() < 4) {
+      await this.openEpisodePage(false);
+      const res = await this.uploadEpisodeThumbnail(backfillAttempts);
+      if (res != 'FAILED') {
+        this.video.addThumbnailUploadAttempt();
+        this.video.clearCache();
+      }
+    } else {
+      log('Skipping image backlog exceeding all automatic text removal attempts');
+    }
   }
 }
 
