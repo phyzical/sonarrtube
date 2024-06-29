3cfbf6386d04e72e8ba0be17ce37105967c5bb23
phyzical <5182053+phyzical@users.noreply.github.com>
2024-06-29 18:48:13 +0800

    rework a bunch of series based functions to increase readbility of caller

--- src/models/api/ActionableSeries.ts
+++ src/models/api/ActionableSeries.ts
@@ -19,6 +19,7 @@ export class ActionableSeries {
     sonarrSeries: SonarrSeries;
     tvdbSeries: TvdbSeries;
     youtubeContext: YoutubeContext;
+    backfillDownloadOnly: boolean = false;
 
     constructor({ sonarrSeries, tvdbSeries, youtubeContext }: ActionableSeriesType) {
         this.youtubeContext = youtubeContext;
@@ -26,37 +27,73 @@ export class ActionableSeries {
         this.sonarrSeries = sonarrSeries;
         this.videos = [];
 
-        if (sonarrSeries.episodes.length != tvdbSeries.episodes.length) {
+        if (this.sonarrSeries.episodes.length != this.tvdbSeries.episodes.length) {
             throw new Error(
                 'Mismatch between tvdb and sonarr episodes!' +
-                `${sonarrSeries.episodes.length} vs ${tvdbSeries.episodes.length}`
+                `${this.sonarrSeries.episodes.length} vs ${this.tvdbSeries.episodes.length}`
             );
         }
 
-        this.tvdbSeries.episodes = tvdbSeries.filterEpisodes();
+        this.tvdbSeries.episodes = this.tvdbSeries.filterEpisodes();
 
         // find eps missing from tvdb, and where they line up
-        for (const video of youtubeContext.videos) {
-            const tvdbEpisode = tvdbSeries
+        for (const video of this.youtubeContext.videos) {
+            const tvdbEpisodes = this.tvdbSeries
                 .episodes
-                .find((episode: TvdbEpisode) => episode.productionCode == video.id);
-            const sonarrEpisode = sonarrSeries
+                .filter((episode: TvdbEpisode) => episode.productionCode == video.id);
+
+            const tvdbEpisode = tvdbEpisodes[0];
+            const sonarrEpisode = this.sonarrSeries
                 .episodes
                 .find((episode: SonarrEpisode) => episode.tvdbId == tvdbEpisode?.id);
+
+            if (tvdbEpisodes.length > 1) {
+                log('Warning found multiple matches this shouldn\'t happen! This is probably a duplicate issue\n'
+                    + 'If you are sure its a duplicate please just set the production code to THIS_WAS_REMOVED for one'
+                );
+                tvdbEpisodes.forEach(episode => {
+                    const tempVideo = new ActionableVideo(
+                        {
+                            youtubeVideo: video, sonarrEpisode,
+                            tvdbEpisode: episode, tvdbSeries: this.tvdbSeries,
+                            sonarrSeries: this.sonarrSeries, youtubeContext: this.youtubeContext
+                        }
+                    );
+                    tempVideo.overviewLog();
+                    tempVideo.clearCache();
+                });
+            }
+
             this.videos.push(
-                new ActionableVideo({ youtubeVideo: video, sonarrEpisode, tvdbEpisode, tvdbSeries, sonarrSeries })
+                new ActionableVideo(
+                    {
+                        youtubeVideo: video, sonarrEpisode,
+                        tvdbEpisode, tvdbSeries: this.tvdbSeries,
+                        sonarrSeries: this.sonarrSeries, youtubeContext: this.youtubeContext
+                    }
+                )
             );
         }
 
-        // find eps with missing prod codes
-        for (const tvdbEpisode of tvdbSeries.episodes.filter(episode => !episode.productionCode)) {
-            const sonarrEpisode = sonarrSeries
-                .episodes
-                .find((episode: SonarrEpisode) => episode.tvdbId == tvdbEpisode?.id);
-
-            this.videos.push(
-                new ActionableVideo({ youtubeVideo: null, sonarrEpisode, tvdbEpisode, tvdbSeries, sonarrSeries })
-            );
+        for (const tvdbEpisode of this.tvdbSeries.episodes) {
+            // find eps with missing prod codes Or missing from youtube context
+            if (
+                !tvdbEpisode.productionCode ||
+                this.youtubeContext.videos.find(video => video.id == tvdbEpisode.productionCode) == null
+            ) {
+                const sonarrEpisode = this.sonarrSeries
+                    .episodes
+                    .find((episode: SonarrEpisode) => episode.tvdbId == tvdbEpisode?.id);
+                this.videos.push(
+                    new ActionableVideo(
+                        {
+                            youtubeVideo: null, sonarrEpisode,
+                            tvdbEpisode, tvdbSeries: this.tvdbSeries,
+                            sonarrSeries: this.sonarrSeries, youtubeContext: this.youtubeContext
+                        }
+                    )
+                );
+            }
         }
 
         this.videos = this.videos
@@ -74,93 +111,116 @@ export class ActionableSeries {
         return this.unDownloadedVideosCached;
     }
 
-    missingFromTvdbVideosCached: ActionableVideo[] = [];
+    // Detected as addable
+    _missingFromTvdbVideos: ActionableVideo[] = null;
     missingFromTvdbVideos(): ActionableVideo[] {
-        if (this.missingFromTvdbVideosCached.length) {
-            return this.missingFromTvdbVideosCached;
-            //  TODO: can we rejig to put this logic in ehre?
-            // .filter(
-            //     video => !this.backfillableVideos()
-            //         .find(backfillVideo => video.youtubeVideo.id === backfillVideo.youtubeVideo.id)
-            // );
+        if (this._missingFromTvdbVideos) {
+            return this._missingFromTvdbVideos;
         }
 
-        this.missingFromTvdbVideosCached = this
+        this._missingFromTvdbVideos = this
             .videos
             .filter(actionableVideo => actionableVideo.missingFromTvdb());
 
-        return this.missingFromTvdbVideosCached;
+        return this._missingFromTvdbVideos;
     }
 
-    missingFromYoutubeVideosCached: ActionableVideo[] = [];
-    missingFromYoutubeVideos(): ActionableVideo[] {
-        if (this.missingFromYoutubeVideosCached.length) {
-            return this.missingFromYoutubeVideosCached;
-        }
-
-        this.missingFromYoutubeVideosCached = this.videos.filter(actionableVideo => actionableVideo.missingYoutube());
-
-        return this.missingFromYoutubeVideosCached;
-    }
-
-    missingProductionCodeTvdbVideosCached: ActionableVideo[] = [];
+    // Detected as missing production codes
+    _missingProductionCodeTvdbVideos: ActionableVideo[] = null;
     missingProductionCodeTvdbVideos(): ActionableVideo[] {
-        if (this.missingProductionCodeTvdbVideosCached.length) {
-            return this.missingProductionCodeTvdbVideosCached;
+        if (this._missingProductionCodeTvdbVideos) {
+            return this._missingProductionCodeTvdbVideos;
         }
 
-        this.missingProductionCodeTvdbVideosCached = this.videos
+        this._missingProductionCodeTvdbVideos = this.videos
             .filter(actionableVideo => actionableVideo.missingProductionCode());
 
-        return this.missingProductionCodeTvdbVideosCached;
+        return this._missingProductionCodeTvdbVideos;
     }
 
-    backfillableVideosCached: ActionableVideo[] = [];
-    backfillableVideos(downloadOnly: boolean = false): ActionableVideo[] {
-        if (this.backfillableVideosCached.length) {
-            return this.backfillableVideosCached;
+    // Detected as not being in the youtube video list
+    _unmatchedYoutubeVideos: ActionableVideo[] = null;
+    unmatchedYoutubeVideos(): ActionableVideo[] {
+        if (this._unmatchedYoutubeVideos) {
+            return this._unmatchedYoutubeVideos;
+        }
+
+        this._unmatchedYoutubeVideos = this.videos
+            .filter(actionableVideo => !actionableVideo.missingProductionCode() && actionableVideo.missingYoutube());
+
+        return this._unmatchedYoutubeVideos;
+    }
+
+    // Detected as updatable with production codes
+    _backfillableProductionCodeVideos: ActionableVideo[] = null;
+    backfillableProductionCodeVideos(downloadOnly: boolean = false): ActionableVideo[] {
+        if (this._backfillableProductionCodeVideos) {
+            return this._backfillableProductionCodeVideos;
         }
 
         const backfillVideos = [];
+        if (downloadOnly) {
+            return backfillVideos;
+        }
 
-        if (!downloadOnly) {
-            for (const episode of this.missingFromYoutubeVideos()) {
-                episode.youtubeVideo = this.youtubeContext
-                    .videos
-                    .find(
-                        (video) => cleanText(video.title()) == cleanText(episode.tvdbEpisode.name) ||
-                            video.airedDate() == episode.tvdbEpisode.aired
-                    );
+        for (const episode of this.missingProductionCodeTvdbVideos()) {
+            episode.youtubeVideo = this.youtubeContext
+                .videos
+                .find(
+                    (video) => cleanText(video.title()) == cleanText(episode.tvdbEpisode.name) ||
+                        cleanText(video.backupTitle()) == cleanText(episode.tvdbEpisode.name) ||
+                        video.airedDate() == episode.tvdbEpisode.aired
+                );
 
-                if (episode.youtubeVideo) {
-                    backfillVideos.push(episode);
-                }
-            }
-
-            for (const episode of this.missingFromTvdbVideos()) {
-                episode.tvdbEpisode = this.tvdbSeries
-                    .episodes
-                    .find(
-                        (tvdbEpisode) =>
-                            cleanText(episode.youtubeVideo.title()) == cleanText(tvdbEpisode.name) ||
-                            episode.youtubeVideo.airedDate() == tvdbEpisode.aired
-                    );
-
-                if (episode.tvdbEpisode) {
-                    backfillVideos.push(episode);
-                }
+            if (episode.youtubeVideo) {
+                backfillVideos.push(episode);
             }
         }
 
-        this.backfillableVideosCached = backfillVideos.filter((video, index, self) =>
+        for (const episode of this.missingFromTvdbVideos()) {
+            episode.tvdbEpisode = this.tvdbSeries
+                .episodes
+                .find(
+                    (tvdbEpisode) =>
+                        cleanText(episode.youtubeVideo.title()) == cleanText(tvdbEpisode.name) ||
+                        cleanText(episode.youtubeVideo.backupTitle()) == cleanText(tvdbEpisode.name) ||
+                        episode.youtubeVideo.airedDate() == tvdbEpisode.aired
+                );
+
+            if (episode.tvdbEpisode) {
+                backfillVideos.push(episode);
+            }
+        }
+
+        this._backfillableProductionCodeVideos = backfillVideos.filter((video, index, self) =>
             index === self.findIndex((v) => v.youtubeVideo.id === video.youtubeVideo.id)
         );
 
-        return this.backfillableVideosCached;
+        return this._backfillableProductionCodeVideos;
+    }
+
+    // Detected as updatable with production codes
+    _backfillableImageVideos: ActionableVideo[] = null;
+    backfillableImageVideos(downloadOnly: boolean = false): ActionableVideo[] {
+        if (this._backfillableImageVideos) {
+            return this._backfillableImageVideos;
+        }
+
+        const backfillVideos = [];
+        if (downloadOnly) {
+            return backfillVideos;
+        }
+
+        this._backfillableImageVideos = this.videos.filter(video =>
+            video.tvdbEpisode && video.youtubeVideo && !video.tvdbEpisode.image
+        );
+
+        return this._backfillableImageVideos;
     }
 
     futureTotal(): number {
-        return this.tvdbSeries.episodes.length + this.missingFromTvdbVideos().length;
+        return this.tvdbSeries.episodes.length +
+            this.missingFromTvdbVideos().length - this.unmatchedYoutubeVideos().length;
     }
 
     hasMissing(): boolean {
@@ -170,30 +230,25 @@ export class ActionableSeries {
             log(
                 `\n${separator}` +
                 `Warning: tvdb count (${this.tvdbSeries.episodes.length}) + ` +
-                `to be added (${this.missingFromTvdbVideos().length}) ${this.futureTotal()} ` +
+                `to be added (${this.missingFromTvdbVideos().length}) - ` +
+                `unmatched with production code (${this.unmatchedYoutubeVideos().length}) ${this.futureTotal()} ` +
                 `!= ${this.youtubeContext.videos.length} current youtube list\n`
             );
 
-            log(`The following are affected (${this.youtubeContext.url});\n(You may need to add these to ignore)\n`);
-            this
-                .tvdbSeries
-                .episodes
-                .map(episode => new ActionableVideo(
-                    {
-                        tvdbEpisode: episode,
-                        youtubeVideo: null,
-                        sonarrEpisode: null,
-                        tvdbSeries: this.tvdbSeries,
-                        sonarrSeries: null
-                    }
-                ))
-                .filter(video => !
-                    this
-                        .youtubeContext
-                        .videos
-                        .find(youtubeVideo => youtubeVideo.id == video.tvdbEpisode.productionCode)
-                )
-                .forEach(video => video.overviewLog());
+            log(
+                [
+                    `The following are affected (${this.youtubeContext.url});`,
+                    // eslint-disable-next-line max-len
+                    '(In the case the video was removed set its production code to THIS_WAS_REMOVED and it will be skipped)',
+                    '(You may need to add these to ignore env)',
+                ].join('\n')
+            );
+
+            this.missingProductionCodeTvdbVideos()//.concat(this.unmatchedYoutubeVideos())
+                .forEach(video => {
+                    video.overviewLog();
+                    video.clearCache();
+                });
 
             log(`\n${separator}`);
         }
