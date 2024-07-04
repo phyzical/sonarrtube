77d88e357d23bceb157fcf94e1555f0161ac14f3
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-04 19:16:03 +0800

    refactor js entrypoint over to ts

---
+++ src/Runner.ts
@@ -0,0 +1,31 @@
+import { ShowSubmitter } from './ShowSubmitter.js';
+import { config } from './helpers/Config.js';
+import { handleSignal } from './helpers/Generic.js';
+
+const rerunInterval = config().reRunInterval;
+
+const nextRunTime = (): Date => new Date((new Date()).getTime() + rerunInterval);
+
+export const run = async (): Promise<void> => {
+  // Listen for termination signals
+  process.on('SIGINT', handleSignal);
+  process.on('SIGTERM', handleSignal);
+
+  let nextRun = nextRunTime();
+  let running = false;
+  const start = async (): Promise<void> => {
+    console.log(`Next run at ${nextRun}`);
+
+    if (!running) {
+      return;
+    }
+    running = true;
+    nextRun = nextRunTime();
+    await new ShowSubmitter().start();
+    running = false;
+  };
+
+  start();
+
+  setInterval(start, rerunInterval);
+};
