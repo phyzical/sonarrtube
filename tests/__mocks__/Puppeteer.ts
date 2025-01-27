7fa7e5629107227db88fde7c2bb6b76667251183
phyzical <5182053+phyzical@users.noreply.github.com>
2025-01-27 14:00:14 +0800

    implement logic to avoid any real requests

---
+++ tests/__mocks__/Puppeteer.ts
@@ -0,0 +1,154 @@
+import { existsSync, readFileSync } from 'fs';
+import { join } from 'path';
+
+import { TvdbSubmitter } from '@sonarrTube/models/submitter/TvdbSubmitter';
+
+export const mockPage = async (tvdbSubmitter: TvdbSubmitter): Promise<void> => {
+    const page = tvdbSubmitter.page();
+
+    await page.setRequestInterception(true);
+
+    page.on('request', (request) => {
+        const url = request.url();
+        let fileName = '';
+        const exclusions = [
+            'delete-reasons',
+            'auth/checkfavorite',
+            '/addseason'
+        ];
+
+        if (url.includes('/token/get')) {
+            request.respond({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    jwt: '123',
+                })
+            });
+
+            return;
+        }
+        if (url.includes('/tvtapi')) {
+            request.respond({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    data: [],
+                    success: 'success',
+                })
+            });
+
+            return;
+        }
+
+        if (url.includes('/users/authenticated')) {
+            request.respond({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    id: 1,
+                    avatar: null,
+                    name: 'test',
+                    level: 'user',
+                })
+            });
+
+            return;
+        }
+
+        if (url.includes('/auth/getuser')) {
+            request.respond({
+                status: 200,
+                contentType: 'application/json',
+                body: JSON.stringify({
+                    id: 1,
+                    name: 'test',
+                    role: 'user',
+                    favorites: { artwork: [] }
+                })
+            });
+
+            return;
+        }
+
+        if (url.includes('/auth/checkpermissions')) {
+            request.respond({
+                status: 200,
+                contentType: 'text/html',
+                body: '1'
+            });
+
+            return;
+        }
+
+        if (exclusions.some((exclusion) => url.includes(exclusion))) {
+            request.abort();
+
+            return;
+        }
+        if ((/\.js|\.css|\.png|\.jp(e)*g|\.webp|\.svg|\/pub-|\.ico/g.test(url))) {
+            fileName = join(
+                __dirname,
+                '..',
+                '__mocks__',
+                'html',
+                'thetvdb.com_files',
+                `${(url.split('/').pop() || '').split('?')[0]}`
+            );
+
+            if (!existsSync(fileName)) {
+                // console.log(`skipped ${fileName}`);
+                request.abort();
+
+                return;
+            }
+
+        } else {
+            if (!/tvdb\.com/g.test(url)) {
+                request.abort();
+
+                return;
+            }
+            let splits = url.split('www.');
+            splits.shift();
+            splits = splits[0].split('/').filter(x => x);
+            fileName = join(__dirname, '..', '__mocks__', 'html', `${splits.join('_')}.html`);
+            if (!existsSync(fileName)) {
+                throw new Error(`Payload not found for ${url}, please save as ${fileName}`);
+            }
+        }
+
+        const body = readFileSync(fileName, 'utf-8');
+        let contentType = '';
+        if (/\.js/g.test(fileName)) {
+            contentType = 'application/javascript';
+        } else if (/\.css/g.test(fileName)) {
+            contentType = 'text/css';
+        } else if (/\.png/g.test(fileName)) {
+            contentType = 'image/png';
+        } else if (/\.jp(e)*g/g.test(fileName)) {
+            contentType = 'image/jpeg';
+        } else if (/\.webp/g.test(fileName)) {
+            contentType = 'image/webp';
+        } else if (/\.svg/g.test(fileName)) {
+            contentType = 'image/svg+xml';
+        } else if (/\.ico/g.test(fileName)) {
+            contentType = 'image/x-icon';
+        } else {
+            contentType = 'text/html';
+        }
+        const headers = request.headers();
+        delete headers['origin'];
+        headers['Access-Control-Allow-Origin'] = '*';
+        request.respond({
+            status: 200,
+            contentType,
+            body,
+            headers
+        });
+
+        return;
+    });
+
+    tvdbSubmitter.pageObj = page;
+};
