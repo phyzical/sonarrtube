ea5c0efac5edd663a4d3b4a152db5351aa546710
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-06 23:16:06 +0800

    add a sonarr api spec

--- tests/__tests__/api/Sonarr.spec.ts
+++ tests/__tests__/api/Sonarr.spec.ts
@@ -1,8 +1,64 @@
+
 import { series } from '@sonarrTube/api/Sonarr';
-describe.skip('Sonarr', () => {
+import { mockConfig } from '@sonarrTube/mocks/Config';
+describe('Sonarr', () => {
+    beforeEach(() => {
+        mockConfig({});
+    });
     describe('series', () => {
-        it('TODO:', async () => {
-            await series();
+        it('it filters out non youtube series, returns the right episodes', async () => {
+            const youtubeSeries = await series();
+            expect(youtubeSeries).toBeArrayOfSize(2);
+            expect(youtubeSeries[0].episodes).toBeArrayOfSize(381);
         });
+
+        describe('filtering Series', () => {
+            beforeEach(() => {
+                mockConfig({
+                    tvdb: {
+                        username: '',
+                        password: '',
+                        email: '',
+                        apiKey: '',
+                        skippedEpisodeIds: [],
+                        matchSeriesIds: [],
+                        skipSeriesIds: [373426]
+                    }
+                });
+            });
+
+            it('it filters out ', async () => {
+
+
+                const youtubeSeries = await series();
+                expect(youtubeSeries).toBeArrayOfSize(1);
+                expect(youtubeSeries[0].episodes).toBeArrayOfSize(381);
+                expect(youtubeSeries[0].id).toBe(8);
+            });
+        });
+
+        describe('Matching season ', () => {
+            beforeEach(() => {
+                mockConfig({
+                    tvdb: {
+                        username: '',
+                        password: '',
+                        email: '',
+                        apiKey: '',
+                        skippedEpisodeIds: [],
+                        matchSeriesIds: [373426],
+                        skipSeriesIds: []
+                    }
+                });
+            });
+
+            it('it filters out ', async () => {
+                const youtubeSeries = await series();
+                expect(youtubeSeries).toBeArrayOfSize(1);
+                expect(youtubeSeries[0].episodes).toBeArrayOfSize(381);
+                expect(youtubeSeries[0].id).toBe(2);
+            });
+        });
+
     });
 });
