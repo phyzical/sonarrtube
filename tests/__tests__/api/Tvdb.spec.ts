41490818b3e8c7467b11b1f1949671a5aa3cf650
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-07 18:43:40 +0800

    add specs to cover the tvdb api

--- tests/__tests__/api/Tvdb.spec.ts
+++ tests/__tests__/api/Tvdb.spec.ts
@@ -1,10 +1,40 @@
 import { series } from '@sonarrTube/api/Tvdb';
 import { seriesFactory } from '@sonarrTube/factories/models/api/sonarr/Series';
-describe.skip('Tvdb', () => {
+import { episodeFactory } from '@sonarrTube/factories/models/api/sonarr/Episode';
+import { consoleSpy } from '@sonarrTube/mocks/Spies';
+describe('Tvdb', () => {
     describe('series', () => {
-        it('should return a list of series', async () => {
-            const serieses = await series([seriesFactory()]);
-            expect(serieses).toBeDefined();
+        it('should return a list of series, should cache request and return same results', async () => {
+            const payload = [
+                seriesFactory(
+                    {
+                        tvdbId: 373426,
+                        episodes: [episodeFactory({ tvdbId: 7546778 }), episodeFactory({ tvdbId: 7546771 })]
+                    }
+                ),
+                seriesFactory(
+                    {
+                        tvdbId: 373427,
+                        episodes: [episodeFactory({ tvdbId: 7546779 }), episodeFactory({ tvdbId: 7546772 })]
+                    }
+                )
+            ];
+            let serieses = await series(payload);
+            expect(consoleSpy).toHaveBeenCalledWith('Logging into tvdb');
+            expect(serieses).toBeArrayOfSize(2);
+            expect(serieses[0].id).toBe(373426);
+            expect(serieses[0].episodes).toBeArrayOfSize(2);
+            expect(serieses[0].episodes[0].id).toBe(7546778);
+
+
+            consoleSpy.mockReset();
+            serieses = await series(payload);
+            expect(consoleSpy).not.toHaveBeenCalledWith('Logging into tvdb');
+            expect(consoleSpy).toHaveBeenCalledWith('Found /tvdb/373426.json in cache. Returning cached data.');
+            expect(serieses).toBeArrayOfSize(2);
+            expect(serieses[0].id).toBe(373426);
+            expect(serieses[0].episodes).toBeArrayOfSize(2);
+            expect(serieses[0].episodes[0].id).toBe(7546778);
         });
     });
 });
