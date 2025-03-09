dc41214233173696b1c98a172310f5ebc47e7615
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-09 19:15:52 +0800

    update mocks

--- tests/config/jest.setup.ts
+++ tests/config/jest.setup.ts
@@ -5,7 +5,7 @@ import { resetCache } from '@sonarrTube/helpers/Cache';
 import { mockConfig, resetConfig, testConfig } from '@sonarrTube/mocks/Config';
 import '@sonarrTube/mocks/Fetch';
 import '@sonarrTube/mocks/Spies';
-import '@sonarrTube/mocks/execSync';
+import '@sonarrTube/mocks/ExecSync';
 
 expect.extend(matchers);
 beforeEach(() => {
